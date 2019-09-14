5b8617fc32e3d951475677c2b7a240c4d509075b
JamesMorales04 <jamesmoralesmoreno@gmail.com>
2019-09-14 17:27:45 -0500

    Proyecto Base

---
+++ BigOrganizer.Classic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BigOrganizer.Classic
+{
+    public class Class1
+    {
+    }
+}
---
+++ BigOrganizer.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BigOrganizer.Common
+{
+    public class Class1
+    {
+    }
+}
